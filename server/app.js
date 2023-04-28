7039c36dbd06076cd1e6a189412e16b67cf20cfc
Lauren Richardson <lauricha@cisco.com>
2023-04-28 13:39:40 -0400

    added views

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-const routes = 
+app.set('view engine', 'ejs')
+
+
 app.get('/',(req,res) => {
-    res.json('Home Page')
+    res.render('home.ejs')
 
 })
 
