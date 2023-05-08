0cb958549965edbb9750d93f102c406fc514ef15
MabeDiaby <94400939+MabeDiaby@users.noreply.github.com>
2023-05-08 13:26:14 -0400

    connecting backend and addding routes

--- server/models/todos.js
+++
@@ -1,8 +0,0 @@
-const { sequelize } = require("../db");
-const { Sequelize } = require("sequelize");
-
-const Todo = sequelize.define("todo", {
-  item: Sequelize.STRING,
-});
-
-module.exports = { Todo };
---
+++ server/routes/index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const dbIn = require('../db');
+
+function fetchTodos(req, res, next) {
+  dbIn.all('SELECT * FROM todos WHERE owner_id = ?', [
+    req.user.id
+  ], function(err, rows) {
+    if (err) { return next(err); }
+    
+    var todos = rows.map(function(row) {
+      return {
+        id: row.id,
+        title: row.title,
+        completed: row.completed == 1 ? true : false,
+        url: '/' + row.id
+      }
+    });
+    res.locals.todos = todos;
+    res.locals.activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
+    res.locals.completedCount = todos.length - res.locals.activeCount;
+    next();
+  });
+}
+
+const inRouter = express.Router();
+
+/* GET home page. */
+inRouter.get('/', function(req, res, next) {
+  if (!req.user) { return res.render('home'); }
+  next();
+}, fetchTodos, function(req, res, next) {
+  res.locals.filter = null;
+  res.render('main', { user: req.user });
+});
+
+inRouter.get('/active', fetchTodos, function(req, res, next) {
+  res.locals.todos = res.locals.todos.filter(function(todo) { return !todo.completed; });
+  res.locals.filter = 'active';
+  res.render('main', { user: req.user });
+});
+
+inRouter.get('/completed', fetchTodos, function(req, res, next) {
+  res.locals.todos = res.locals.todos.filter(function(todo) { return todo.completed; });
+  res.locals.filter = 'completed';
+  res.render('main', { user: req.user });
+});
+
+inRouter.post('/', function(req, res, next) {
+  req.body.title = req.body.title.trim();
+  next();
+}, function(req, res, next) {
+  if (req.body.title !== '') { return next(); }
+  return res.redirect('/' + (req.body.filter || ''));
+}, function(req, res, next) {
+  dbIn.run('INSERT INTO todos (owner_id, title, completed) VALUES (?, ?, ?)', [
+    req.user.id,
+    req.body.title,
+    req.body.completed == true ? 1 : null
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+inRouter.post('/:id(\\d+)', function(req, res, next) {
+  req.body.title = req.body.title.trim();
+  next();
+}, function(req, res, next) {
+  if (req.body.title !== '') { return next(); }
+  dbIn.run('DELETE FROM todos WHERE id = ? AND owner_id = ?', [
+    req.params.id,
+    req.user.id
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+}, function(req, res, next) {
+  dbIn.run('UPDATE todos SET title = ?, completed = ? WHERE id = ? AND owner_id = ?', [
+    req.body.title,
+    req.body.completed !== undefined ? 1 : null,
+    req.params.id,
+    req.user.id
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+inRouter.post('/:id(\\d+)/delete', function(req, res, next) {
+  dbIn.run('DELETE FROM todos WHERE id = ? AND owner_id = ?', [
+    req.params.id,
+    req.user.id
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+inRouter.post('/toggle-all', function(req, res, next) {
+  dbIn.run('UPDATE todos SET completed = ? WHERE owner_id = ?', [
+    req.body.completed !== undefined ? 1 : null,
+    req.user.id
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+inRouter.post('/clear-completed', function(req, res, next) {
+  dbIn.run('DELETE FROM todos WHERE owner_id = ? AND completed = ?', [
+    req.user.id,
+    1
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+module.exports = inRouter;
